e4130329875e1aa75903652f92951dfa19f02569
digschauhan <digs.chauhan@gmail.com>
2017-04-12 10:34:08 -0600

    Initial project structure for Todo Project

---
+++ src/main/java/com/ddworld/todo/MyTodoProjectApplication.java
@@ -0,0 +1,12 @@
+package com.ddworld.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTodoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTodoProjectApplication.class, args);
+	}
+}
